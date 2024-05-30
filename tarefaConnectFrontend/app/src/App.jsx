e2875b19d628fe93afec0dd43e7e30696357a84b
Riley <rh1122@ic.ac.uk>
2024-05-30 15:30:23 +0100

    [feat] added Header element

--- tarefaConnectFrontend/app/src/App.jsx
+++ tarefaConnectFrontend/app/src/App.jsx
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './header/Header';
+import LoginLanding from './loginLanding/LoginLanding';
+import styles from './Global'
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 function App() {
+  const mainStyle = styles().fonts.paragraph;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="App" style={{fontFamily:mainStyle}}>
+      <Header />
+      <LoginLanding />
+    </main>
   );
 }
 
