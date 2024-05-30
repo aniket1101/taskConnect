72733d5a8d8ceb08726cc800201a24b4123494b4
Gabriel Costa <gc1122@ic.ac.uk>
2024-05-30 18:22:07 +0100

    fix: fix to docker and connection between front and back.

--- tarefaConnectBackend/backend/main.py
+++ tarefaConnectBackend/backend/main.py
@@ -1,6 +1,7 @@
 from typing import Generator
 
 from fastapi import Depends, FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from sqlalchemy.orm import Session
 
@@ -10,6 +11,18 @@ from .database import SessionLocal, engine
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+origins = [
+    'http://localhost:3000'
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 
 # Dependency
