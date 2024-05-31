290441236dc227baac0e8a5f94e40c7439a7f7aa
Gabriel Costa <gc1122@ic.ac.uk>
2024-05-31 10:25:34 +0100

    fix: mounted frontend at /.

--- tarefaConnectBackend/backend/main.py
+++ tarefaConnectBackend/backend/main.py
@@ -2,6 +2,7 @@ from typing import Generator
 
 from fastapi import Depends, FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
 from sqlalchemy.orm import Session
 
@@ -12,23 +13,23 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-origins = [
-    'http://localhost:3000'
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"]
-)
+# origins = [
+#     'http://localhost:3000'
+# ]
+#
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"]
+# )
 
 
 # Dependency
 def get_db() -> Generator:
+    db = SessionLocal()
     try:
-        db = SessionLocal()
         yield db
     finally:
         db.close()
@@ -52,3 +53,6 @@ def create_task(new_task: schemas.TaskCreate, db: Session = Depends(get_db)):
 @app.get("/tasks/{task_id}", response_model=schemas.Task)
 def get_task(task_id: int, db: Session = Depends(get_db)):
     return crud.get_task(db, task_id)
+
+
+app.mount("/", StaticFiles(directory="../../tarefaConnectFrontend/app/build", html=True), name="frontend")
