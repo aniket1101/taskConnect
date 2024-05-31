14c07643935b2829828801978ceb95220cb5bff5
rileyhorrix <rh1122@ic.ac.uk>
2024-05-31 14:53:30 +0100

    [feat] added a GET request for task data in task page

--- tarefaConnectFrontend/app/src/App.jsx
+++ tarefaConnectFrontend/app/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 import Header from './header/Header';
 import LoginLanding from './loginLanding/LoginLanding';
 import Task from './task/Task';
+import axios from 'axios';
 
 import {
   BrowserRouter as Router,
@@ -10,6 +11,13 @@ import {
 } from "react-router-dom";
 import styles from './Global'
 import "bootstrap-icons/font/bootstrap-icons.css";
+
+export const api = axios.create({
+  baseURL: 'http://tarefaconnect.doc.ic.ac.uk/api/',
+  headers: {
+    'Content-Type': 'application/json'
+  }
+})
 
 function App() {
   const mainStyle = styles().fonts.paragraph;
--- tarefaConnectFrontend/app/src/task/Task.jsx
+++ tarefaConnectFrontend/app/src/task/Task.jsx
@@ -2,56 +2,67 @@ import "./panelStyle.css";
 
 import CreateTask from "./create/CreateTask";
 import TaskDisplay from "./display/TaskDisplay"
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { api } from "../App";
 
 export default function Task({ startingIndex = -1 }) {
-    const exampleTasks = [
-        {
-            title: 'Gardening Every Week',
-            description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusamus dolores minus, quo fuga possimus iure iusto commodi voluptatibus, aut architecto nesciunt est amet reiciendis quas odit suscipit laudantium quis hic.'
-        },
-        {
-            title: "Broken Toilet Seat",
-            description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusamus dolores minus, quo fuga possimus iure iusto commodi voluptatibus, aut architecto nesciunt est amet reiciendis quas odit suscipit laudantium quis hic.'
-        },
-        {
-            title: "Faulty Light Bulb",
-            description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusamus dolores minus, quo fuga possimus iure iusto commodi voluptatibus, aut architecto nesciunt est amet reiciendis quas odit suscipit laudantium quis hic.'
-        }
-    ]
-    const [index, setIndex] = useState(startingIndex);
-    // eslint-disable-next-line
-    const [taskData, setTaskData] = useState(exampleTasks);
-    return (
-        <div className="Container">
-            <CurrentTaskPanel changeIndex={setIndex} data={taskData.map((item, index) => [item.title, index])} />
-            <TaskContentPanel index={index} data={taskData} />
-        </div>
-    );
+  const exampleTasks = [
+    {
+      title: 'Gardening Every Week',
+      description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusamus dolores minus, quo fuga possimus iure iusto commodi voluptatibus, aut architecto nesciunt est amet reiciendis quas odit suscipit laudantium quis hic.'
+    },
+    {
+      title: "Broken Toilet Seat",
+      description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusamus dolores minus, quo fuga possimus iure iusto commodi voluptatibus, aut architecto nesciunt est amet reiciendis quas odit suscipit laudantium quis hic.'
+    },
+    {
+      title: "Faulty Light Bulb",
+      description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusamus dolores minus, quo fuga possimus iure iusto commodi voluptatibus, aut architecto nesciunt est amet reiciendis quas odit suscipit laudantium quis hic.'
+    }
+  ]
+  const [index, setIndex] = useState(startingIndex);
+  const [taskData, setTaskData] = useState(exampleTasks);
+
+  useEffect(() => {
+    api.get("create-task")
+      .then(data => {
+        setTaskData(data.data);
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }, [])
+
+  return (
+    <div className="Container">
+      <CurrentTaskPanel changeIndex={setIndex} data={taskData.map((item, index) => [item.title, index])} />
+      <TaskContentPanel index={index} data={taskData} />
+    </div>
+  );
 }
 
 function TaskContentPanel({ index, data }) {
-    return (index === -1 ? <CreateTask /> : <TaskDisplay taskData={data[index]} />);
+  return (index === -1 ? <CreateTask /> : <TaskDisplay taskData={data[index]} />);
 }
 
 function CurrentTaskPanel({ changeIndex, data }) {
-    return (
-        <div className="LeftPanel">
-            <h1 className="CurrentTaskTitle">
-                Your Current Tasks
-            </h1>
-            <hr></hr>
-            <div className="CurrentTasks">
-                {data.map(([title, index]) => {
-                    return (
-                        <div key={index}>
-                            <button className="CurrentTaskButton" onClick={() => { changeIndex(index) }}>
-                                {title}
-                            </button>
-                        </div>
-                    );
-                })}
+  return (
+    <div className="LeftPanel">
+      <h1 className="CurrentTaskTitle">
+        Your Current Tasks
+      </h1>
+      <hr></hr>
+      <div className="CurrentTasks">
+        {data.map(([title, index]) => {
+          return (
+            <div key={index}>
+              <button className="CurrentTaskButton" onClick={() => { changeIndex(index) }}>
+                {title}
+              </button>
             </div>
-        </div>
-    );
+          );
+        })}
+      </div>
+    </div>
+  );
 }
