b37d4709189144774d4d566ea7e3c1df791ed419
rileyhorrix <rh1122@ic.ac.uk>
2024-05-31 16:18:40 +0100

    [feat] code review commit (REVERT)

--- tarefaConnectFrontend/app/src/header/Header.jsx
+++ tarefaConnectFrontend/app/src/header/Header.jsx
@@ -32,29 +32,29 @@ export default function Header() {
   const style = styles();
 
   return (
-  <div className="Header" style={{backgroundColor:style.colours.backgroundPrimary, borderBottom:'1px solid ' + style.colours.tertiary}}>
+    <div className="Header" style={{ backgroundColor: style.colours.backgroundPrimary, borderBottom: '1px solid ' + style.colours.tertiary }}>
 
-    <MenuSection colour={style.colours.primary} data={leftHeader} />
+      <MenuSection colour={style.colours.primary} data={leftHeader} />
 
-    <h1 style={{margin:'auto', color:style.colours.primary, flexGrow:1, fontFamily:style.fonts.heading}}>
-      TarefaConnect
-    </h1>
+      <h1 style={{ margin: 'auto', color: style.colours.primary, flexGrow: 1, fontFamily: style.fonts.heading }}>
+        TarefaConnect(hi mark)
+      </h1>
 
-    <MenuSection colour={style.colours.primary} data={rightHeader} />
-  </div>
-);
+      <MenuSection colour={style.colours.primary} data={rightHeader} />
+    </div>
+  );
 }
 
-function MenuSection({colour, data}) {
+function MenuSection({ colour, data }) {
   return (
     <div className='MenuSection'>
       {data.map((item) => {
         return (
           <div key={item.key} className={item.iconClass}>
-            <i style={{color:colour}} className={item.class}></i>
+            <i style={{ color: colour }} className={item.class}></i>
           </div>
         )
-        })}
+      })}
     </div>
   );
 }
