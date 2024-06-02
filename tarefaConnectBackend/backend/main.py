351ae3a60dfb9674ffb1bfe7cd1742faf062d006
Gabriel Costa <gc1122@ic.ac.uk>
2024-06-02 14:19:40 +0100

    [fix]: made limit a non required argument.

--- tarefaConnectBackend/backend/main.py
+++ tarefaConnectBackend/backend/main.py
@@ -38,7 +38,7 @@ def login_user(user_details: schemas.UserLogin, db: Session = Depends(get_db)):
 
 
 @app.get("/api/{user_id}/tasks", response_model=list[schemas.Task])
-def get_user_tasks(user_id: int, limit: int | None, db: Session = Depends(get_db)):
+def get_user_tasks(user_id: int, limit: int | None = None, db: Session = Depends(get_db)):
     db_user = crud.get_user(db, user_id=user_id)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
