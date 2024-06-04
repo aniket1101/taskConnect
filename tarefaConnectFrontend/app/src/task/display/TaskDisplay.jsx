fedf98cfc2d6cb913ccf00812d4056c76379617c
rileyhorrix <rh1122@ic.ac.uk>
2024-06-04 12:52:56 +0100

    [feat] added gmaps api call into display tasks

--- tarefaConnectFrontend/app/src/task/display/TaskDisplay.jsx
+++ tarefaConnectFrontend/app/src/task/display/TaskDisplay.jsx
@@ -1,92 +1,97 @@
 import './TaskDisplay.css';
 
+import { Map, APIProvider } from '@vis.gl/react-google-maps';
+
 export default function TaskDisplay({ taskData }) {
-    return (
-        <div className="RightContainer">
-            <div className="TaskContainer">
-                <TaskInfoPanel taskData={taskData} />
-                <TaskLocationPanel />
-            </div>
-            <TaskRepliesPanel />
-        </div>
-    );
+  return (
+    <div className="RightContainer">
+      <div className="TaskContainer">
+        <TaskInfoPanel taskData={taskData} />
+        <TaskLocationPanel />
+      </div>
+      <TaskRepliesPanel />
+    </div>
+  );
 }
 
 function TaskLocationPanel() {
-    return (
-        <div className="TaskLocationPanel">
-            <label> LOCATION </label>
-        </div>
-    );
+  return (
+    <APIProvider apiKey={'AIzaSyB5bZTy-YbZGqvue8X2XPUO3jTiIul-zmY'} onLoad={() => console.log('Maps API has loaded.')}>
+      <Map defaultZoom={7.5}
+        defaultCenter={{ lat: 38.205542, lng: -28.284108 }}
+      >
+      </Map>
+    </APIProvider>
+  );
 }
 
 function TaskInfoPanel({ taskData }) {
-    return (
-        <div className="TaskInfoPanel">
-            <h1 className="TaskName"> {taskData.title} </h1>
-            <dd className="TaskDescription">
-                {taskData.description}
-            </dd>
-        </div>
-    );
+  return (
+    <div className="TaskInfoPanel">
+      <h1 className="TaskName"> {taskData.title} </h1>
+      <dd className="TaskDescription">
+        {taskData.description}
+      </dd>
+    </div>
+  );
 }
 
 function TaskRepliesPanel() {
-    return (
-        // Conditionally render this depending on # of replies
-        <div className="TaskRepliesPanel">
-            <h1 className="RepliesHeader">Replies from Taskers!</h1>
-            <RepliesSection />
-        </div>
-    );
+  return (
+    // Conditionally render this depending on # of replies
+    <div className="TaskRepliesPanel">
+      <h1 className="RepliesHeader">Replies from Taskers!</h1>
+      <RepliesSection />
+    </div>
+  );
 }
 
 const repliesData = [
-    {
-        name: " Casanova",
-        rating: 5,
-        message: "Hey! I am an experienced gardener from Sao Miguel. I have many clients on the island and would love to add your garden to my clients!"
-    },
-    {
-        name: "Brand Grent",
-        rating: 4,
-        message: "Hello! I love gardening and would love to get involved!"
-    }
+  {
+    name: " Casanova",
+    rating: 5,
+    message: "Hey! I am an experienced gardener from Sao Miguel. I have many clients on the island and would love to add your garden to my clients!"
+  },
+  {
+    name: "Brand Grent",
+    rating: 4,
+    message: "Hello! I love gardening and would love to get involved!"
+  }
 ];
 
 function RepliesSection() {
-    const replies = repliesData.map((item, index) => {
-        return (
-            <div className="ReplyElement" key={index}>
-                <div className="ReplyNameContainer">
-                    <label className="ReplyName"> {item.name} </label>
-                    <div className="star-rating">
-                        <div className="stars-container">
-                            <StarDisplay number={item.rating} />
-                        </div>
-                    </div>
-                </div>
-                <div className="Divider"></div>
-                <div className="ReplyMessageContainer">
-                    <dd className="ReplyMessage"> {item.message} </dd>
-                </div>
+  const replies = repliesData.map((item, index) => {
+    return (
+      <div className="ReplyElement" key={index}>
+        <div className="ReplyNameContainer">
+          <label className="ReplyName"> {item.name} </label>
+          <div className="star-rating">
+            <div className="stars-container">
+              <StarDisplay number={item.rating} />
             </div>
-        );
-    })
-    return <div> {replies} </div>
+          </div>
+        </div>
+        <div className="Divider"></div>
+        <div className="ReplyMessageContainer">
+          <dd className="ReplyMessage"> {item.message} </dd>
+        </div>
+      </div>
+    );
+  })
+  return <div> {replies} </div>
 }
 
 function StarDisplay({ number }) {
-    var stars = [];
-    for (let i = 0; i < 5 - number; i++) {
-        stars.push(
-            <div className="star" key={i}>&#9733;</div>
-        )
-    }
-    for (let i = 0; i < number; i++) {
-        stars.push(
-            <div className="golden-star" key={i + 100}>&#9733;</div>
-        )
-    }
-    return <div> {stars} </div>
+  var stars = [];
+  for (let i = 0; i < 5 - number; i++) {
+    stars.push(
+      <div className="star" key={i}>&#9733;</div>
+    )
+  }
+  for (let i = 0; i < number; i++) {
+    stars.push(
+      <div className="golden-star" key={i + 100}>&#9733;</div>
+    )
+  }
+  return <div> {stars} </div>
 }
