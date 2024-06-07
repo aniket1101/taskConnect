752cdbf1481623194ea7f68a1586c5597ebe364b
aniket1101 <1.aniketgupta@gmail.com>
2024-06-07 13:41:22 +0100

    [feat] Added icons following HCD

--- tarefaConnectFrontend/app/src/loginLanding/LoginLanding.jsx
+++ tarefaConnectFrontend/app/src/loginLanding/LoginLanding.jsx
@@ -1,5 +1,7 @@
 import { LandingPage } from './LandingUtils';
 import './LoginLanding.css';
+import handHoldingService from '../assets/handHoldingService.svg'
+import personRaisingHand from '../assets/personRaisingHand.svg' 
 
 // eslint-disable-next-line
 const panelData = [
@@ -7,12 +9,14 @@ const panelData = [
     id: 0,
     buttonText: "I need help with something",
     body: "Get help from a worker to complete your job",
+    icon: personRaisingHand,
     redirectTo: "/findHelp"
   },
   {
     id: 1,
     buttonText: "I provide a service",
     body: "See what people need doing and negotiate a contract",
+    icon: handHoldingService,
     redirectTo: "/taskList"
   }
 ]
