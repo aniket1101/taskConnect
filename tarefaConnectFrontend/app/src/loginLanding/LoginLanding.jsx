46913b27d57ce7184ca254f4e673f5c052bb1134
rileyhorrix <rh1122@ic.ac.uk>
2024-06-17 13:42:01 +0100

    [fix] fixed panel rendering

--- tarefaConnectFrontend/app/src/loginLanding/LoginLanding.jsx
+++ tarefaConnectFrontend/app/src/loginLanding/LoginLanding.jsx
@@ -4,26 +4,25 @@ import earnMoney from '../assets/earnMoney.svg'
 import postTask from '../assets/postTask.svg'
 
 // eslint-disable-next-line
-const panelData = [
-  {
-    id: 3,
-    buttonText: "Let Someone Find You",
-    body: "Create a post and you'll receive messages from workers interested in helping you",
-    icon: postTask,
-    redirectTo: "/task",
-    state: { startingId: -1 }
-  },
-  {
-    id: 1,
-    buttonText: "Want to start earning?",
-    body: "Reach out to people and get paid to help",
-    icon: earnMoney,
-    redirectTo: "/taskList",
-    state: {}
-  }
-]
+const userPanel =
+{
+  id: 3,
+  buttonText: "Let Someone Find You",
+  body: "Create a post and you'll receive messages from workers interested in helping you",
+  icon: postTask,
+  redirectTo: "/task",
+  state: { startingId: -1 }
+};
+const taskerPanel =
+{
+  id: 1,
+  buttonText: "Want to start earning?",
+  body: "Reach out to people and get paid to help",
+  icon: earnMoney,
+  redirectTo: "/taskList",
+  state: {}
+};
 
 export default function LoginLanding({ isTasker }) {
-  console.log('is tasker? ', isTasker);
-  return LandingPage(panelData.slice(0, isTasker ? panelData.length - 1 : 0));
+  return LandingPage(isTasker ? [userPanel, taskerPanel] : [userPanel]);
 }
